164ddffb0c0a9bd40f863a66e5a86aae262d082d
AleksandarD7 <aleksandar.dimovski09@gmail.com>
2023-09-21 17:22:55 +0200

    added tictactoe game

--- script.js
+++ script.js
@@ -45,16 +45,16 @@ const handleGameOver = () => {
 }
 
 const changeDirection = e => {
-    if(e.key === "ArrowUp" && velocityY != 1) {
+    if (e.key === "ArrowUp" && velocityY != 1) {
         velocityX = 0;
         velocityY = -1;
-    } else if(e.key === "ArrowDown" && velocityY != -1) {
+    } else if (e.key === "ArrowDown" && velocityY != -1) {
         velocityX = 0;
         velocityY = 1;
-    } else if(e.key === "ArrowLeft" && velocityX != 1) {
+    } else if (e.key === "ArrowLeft" && velocityX != 1) {
         velocityX = -1;
         velocityY = 0;
-    } else if(e.key === "ArrowRight" && velocityX != -1) {
+    } else if (e.key === "ArrowRight" && velocityX != -1) {
         velocityX = 1;
         velocityY = 0;
     }
@@ -63,13 +63,13 @@ const changeDirection = e => {
 controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
 
 const initGame = () => {
-    if(gameOver) return handleGameOver();
+    if (gameOver) return handleGameOver();
     let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
-    if(snakeX === foodX && snakeY === foodY) {
+    if (snakeX === foodX && snakeY === foodY) {
         updateFoodPosition();
-        snakeBody.push([foodY, foodX]); 
-        score++; 
+        snakeBody.push([foodY, foodX]);
+        score++;
         highScore = score >= highScore ? score : highScore;
         localStorage.setItem("high-score", highScore);
         scoreElement.innerText = `Score: ${score}`;
@@ -77,13 +77,13 @@ const initGame = () => {
     }
     snakeX += velocityX;
     snakeY += velocityY;
-    
+
     for (let i = snakeBody.length - 1; i > 0; i--) {
         snakeBody[i] = snakeBody[i - 1];
     }
-    snakeBody[0] = [snakeX, snakeY]; 
+    snakeBody[0] = [snakeX, snakeY];
 
-    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
         return gameOver = true;
     }
 
@@ -104,7 +104,69 @@ document.addEventListener("keyup", changeDirection);
 
 
 // Script za TicTacToe
+
 let playerSymbol = "X";
 let gameEnded = false
 
+let winPos = [
+  [1, 2, 3], [4, 5, 6], 
+  [7, 8, 9], [1, 4, 7], 
+  [2, 5, 8], [3, 6, 9], 
+  [1, 5, 9], [3, 5, 7]
+];
 
+for (let i = 1; i <= 9; i++) {
+  // Whenever a player clicks on a cell
+  document.getElementById(i.toString()).addEventListener(
+    "click", 
+    function() {
+      if (this.innerHTML === "" && !gameEnded) {
+        // Display either "X" or "O" in the cell, and style it
+        this.innerHTML = playerSymbol;
+        this.classList.add(playerSymbol.toLowerCase());
+
+        // Check if a player has won
+        checkWin();
+        
+        // Swap the symbol to the other one for the next turn
+        if (playerSymbol === "X")
+          playerSymbol = "O"
+        else
+          playerSymbol = "X"
+      }
+    }
+  );
+}
+
+function checkWin() {
+  for (let i = 0; i < winPos.length; i++) {
+	if (
+	  document.getElementById(winPos[i][0]).innerHTML === playerSymbol &&
+	  document.getElementById(winPos[i][1]).innerHTML === playerSymbol &&
+	  document.getElementById(winPos[i][2]).innerHTML === playerSymbol
+	) { 
+    console.log(winPos[i][0]);
+
+	  document.getElementById(winPos[i][0]).classList.add("win");
+	  document.getElementById(winPos[i][1]).classList.add("win");
+	  document.getElementById(winPos[i][2]).classList.add("win"); 
+	  gameEnded = true;
+	  setTimeout(function() {
+	    alert(playerSymbol + " wins!");
+	  }, 500);
+	}
+  }
+}
+
+document.getElementById("reset").addEventListener(
+  "click", 
+  function() {
+    for (let i = 1; i <= 9; i++) {
+      document.getElementById(i.toString()).innerHTML = "";
+      document.getElementById(i.toString()).classList.remove("x");
+      document.getElementById(i.toString()).classList.remove("o");
+      document.getElementById(i.toString()).classList.remove("win");
+      gameEnded = false;
+    }
+  }
+);
